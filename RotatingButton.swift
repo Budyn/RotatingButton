305ae1a339b4e75f007822d857eed5d9227ee544
Daniel Budynski <daniel@prismake.com>
2018-05-01 20:27:53 +0200

    All sources

---
+++ RotatingButton.swift
@@ -0,0 +1,37 @@
+// RotatingButton
+// Created by Daniel Budyński 👾
+// https://github.com/Budyn/
+// MacOS 10.13, Swift 4.0
+
+import UIKit
+
+class RotatingButton: UIButton {
+
+    func rotate(time: Double = 0.7) {
+        UIView.animateKeyframes(withDuration: time,
+                                delay: 0.0,
+                                options: [],
+                                animations: {
+                                    self.prepareAnimation()
+                                }, completion: { _ in
+                                    self.finishAnimation()
+                                })
+    }
+
+    private func prepareAnimation() {
+        UIView.addKeyframe(withRelativeStartTime: 0,
+                           relativeDuration: 0.5,
+                           animations: {
+                            self.transform = CGAffineTransform(rotationAngle: -.pi)
+        })
+        UIView.addKeyframe(withRelativeStartTime: 0.5,
+                           relativeDuration: 0.5,
+                           animations: {
+                            self.transform = CGAffineTransform(rotationAngle: -.pi * 2)
+        })
+    }
+
+    private func finishAnimation() {
+        self.transform = .identity
+    }
+}
